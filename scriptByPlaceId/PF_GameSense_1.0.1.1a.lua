8dbf12d3af5995da1d11f3a14fb455c99240b38d
Shawn <87413601+shawnjbragdon-0@users.noreply.github.com>
2021-07-25 21:36:24 -0700

    GameSense notification
    Co-Authored-By: Shawn <70822166+Luminential@users.noreply.github.com>

--- scriptByPlaceId/PF_GameSense_1.0.1.1a.lua
+++ scriptByPlaceId/PF_GameSense_1.0.1.1a.lua
@@ -1,4 +1,7 @@
 -- You really thought you found my valuable script?
 -- I made this one for myself, but I'll put an obfuscated version here later.
 
+coroutine.wrap(rconsoleclear)()
+coroutine.wrap(rconsoleprint)("GameSense Version 1.0.1.1-a is currently released but experimental features are currently in testing.\n\tContact shawnjbragdon#0001 for more information on GameSense & it\'s functionality (questions only, please).")
+
 return false, "GameSense is currently not available.";
